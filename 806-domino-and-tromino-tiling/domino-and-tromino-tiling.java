2589748e1d0fc8f393c51d03ce89e214110c0265
Gunjan Nimbalkar <103045206+Gunjan-2002@users.noreply.github.com>
2023-08-04 11:31:12 +0530

    Time: 0 ms (100.00%) | Memory: 39 MB (95.93%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numTilings(int n) {
+        int dp[] = new int[n+1];
+        dp[1]=1;
+
+        if(n > 1){
+            dp[2]=2;
+        }
+
+        if(n > 2){
+            dp[3]=5;
+        }
+
+       
+
+        int mod = 1000000007;
+
+        for(int i=4;i<=n;i++){
+            dp[i] = (2*dp[i-1]%mod + dp[i-3]%mod)%mod;
+        }
+
+        return dp[n];
+    }
+}
