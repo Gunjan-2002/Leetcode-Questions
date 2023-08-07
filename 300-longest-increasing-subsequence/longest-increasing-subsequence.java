3c3bed158fafd49344dc00bd38f68534d49c64c5
Gunjan Nimbalkar <103045206+Gunjan-2002@users.noreply.github.com>
2023-08-07 16:12:44 +0530

    Time: 10 ms (78.51%) | Memory: 43.1 MB (74.78%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        TreeSet<Integer> bst = new TreeSet<>();
+        for(Integer num : nums){
+            Integer higherOrEqual = bst.ceiling(num);
+            if(higherOrEqual == null){
+                bst.add(num);
+            }else{
+                bst.remove(higherOrEqual);
+                bst.add(num);
+            }
+        }
+        return bst.size();
+    }
+}
