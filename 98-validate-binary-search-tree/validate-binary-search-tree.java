55aa5f5a527dab72a8afde7012b12cd6e9326448
Gunjan Nimbalkar <103045206+Gunjan-2002@users.noreply.github.com>
2023-08-10 22:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (22.77%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode pre = null;
+    public boolean isValidBST(TreeNode root) {
+        if(root != null){
+            if(!isValidBST(root.left)){
+                return false;
+            }
+
+            if(pre != null && root.val <= pre.val){
+                return false;
+            }
+
+            pre = root;
+
+            return isValidBST(root.right);
+        }
+        else{
+            return true;
+        }
+    }
+}
