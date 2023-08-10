c6d7292e2cc6541d9c6e8deb15edd58e0a870f82
Gunjan Nimbalkar <103045206+Gunjan-2002@users.noreply.github.com>
2023-08-10 13:54:18 +0530

    Time: 2 ms (42.24%) | Memory: 43.9 MB (7.19%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -16,31 +16,31 @@
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         int maxWidth = 0;
-        Map<TreeNode, Integer> map = new HashMap<>();
-        map.put(root,0);
+        Map<TreeNode,Integer> m = new HashMap<>();
+        m.put(root,0);
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
 
         while(!q.isEmpty()){
             int size = q.size();
-            int left = map.get(q.peek());
+            int left = m.get(q.peek());
 
             while(size-- > 0){
-                TreeNode node = q.poll();
-                maxWidth = Math.max(maxWidth, map.get(node) - left + 1);
+                TreeNode curr = q.poll();
+                maxWidth = Math.max(maxWidth, m.get(curr) - left + 1);
 
-                if(node.left != null){
-                    q.offer(node.left);
-                    map.put(node.left, map.get(node)*2);
+                if(curr.left != null){
+                    q.offer(curr.left);
+                    m.put(curr.left,m.get(curr)*2);
                 }
 
-                if(node.right != null){
-                    q.offer(node.right);
-                    map.put(node.right, map.get(node)*2+1);
+                if(curr.right != null){
+                    q.offer(curr.right);
+                    m.put(curr.right,m.get(curr)*2 + 1);
                 }
             }
-        }
 
+        }
         return maxWidth;
     }
 }
