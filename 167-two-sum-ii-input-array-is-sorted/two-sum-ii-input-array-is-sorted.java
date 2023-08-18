921afaa58225a1acf8c5184bf4e2aacabe71d625
Gunjan Nimbalkar <103045206+Gunjan-2002@users.noreply.github.com>
2023-08-18 23:07:47 +0530

    Time: 6 ms (12.99%) | Memory: 45.7 MB (34.55%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,30 +1,30 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        // HashMap<Integer,Integer> h  = new HashMap<>();
+        HashMap<Integer,Integer> h  = new HashMap<>();
         // h.put(0,0);
 
-        // for(int i=0;i<numbers.length;i++){
-        //     if(h.containsKey(target-numbers[i])){
-        //         return new int[]{ h.get(target-numbers[i]) , i+1 };
-        //     }
-        //     h.put(numbers[i],1);
-        // }
-        // return new int[]{0,0};
-
-        int i=0;
-        int j=numbers.length-1;
-
-        while(i < j){
-            if(numbers[i]+numbers[j] == target){
-                return new int[]{ i+1,j+1};
+        for(int i=0;i<numbers.length;i++){
+            if(h.containsKey(target-numbers[i])){
+                return new int[]{ h.get(target-numbers[i]) , i+1 };
             }
-
-            if(numbers[i]+numbers[j] > target){
-                j--;
-            }else{
-                i++;
-            }
+            h.put(numbers[i],i+1);
         }
         return new int[]{0,0};
+
+        // int i=0;
+        // int j=numbers.length-1;
+
+        // while(i < j){
+        //     if(numbers[i]+numbers[j] == target){
+        //         return new int[]{ i+1,j+1};
+        //     }
+
+        //     if(numbers[i]+numbers[j] > target){
+        //         j--;
+        //     }else{
+        //         i++;
+        //     }
+        // }
+        // return new int[]{0,0};
     }
 }
